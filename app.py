bd9b3ac4eee8184a18d236bd6f5ac193303c4b7c
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-04-26 12:22:51 -0700

    starter pack instructions

--- app.py
+++ app.py
@@ -2,13 +2,25 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from models import connect_db, db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.get("/")
+def show_homePage():
+   """Display Home Page"""
+
+   return render_template("home.html")
+
+# @app.post("/")
+# def something():
+#    """Fill in later"""
